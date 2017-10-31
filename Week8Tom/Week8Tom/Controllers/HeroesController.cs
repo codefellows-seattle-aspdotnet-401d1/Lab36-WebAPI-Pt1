2d356e00189f02502d3e929e3dfbb7da2d4015cb
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-31 10:38:40 -0700

    created heroes controller

---
+++ Week8Tom/Week8Tom/Controllers/HeroesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Week8Tom.Controllers
+{
+    [Route("api/heroes")]
+    public class HeroesController : ControllerBase
+    {
+
+    }
+}
