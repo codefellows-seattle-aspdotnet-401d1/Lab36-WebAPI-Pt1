7fd37fe8ef6b4de96eb3e6e157bae90140cb2e5c
George Gaylor <georgegaylor83@outlook.com>
2017-10-31 10:46:00 -0700

    Initial scaffolding

---
+++ Lab36George/Lab36GeorgeTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+using Lab36George
+
+namespace Lab36GeorgeTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
