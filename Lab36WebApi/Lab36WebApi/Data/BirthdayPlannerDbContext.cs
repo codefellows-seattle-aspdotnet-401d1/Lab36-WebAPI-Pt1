354daff18aa08fd2ed1b78bd8203cd695f2ecc0d
duane rush <desertduane@yahoo.com>
2017-10-31 09:25:20 -0700

    api set up folders created

---
+++ Lab36WebApi/Lab36WebApi/Data/BirthdayPlannerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Lab36WebApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab36WebApi.Data
+{
+    public class BirthdayPlannerDbContext : DbContext
+    {
+        public BirthdayPlannerDbContext(DbContextOptions<BirthdayPlannerDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<BirthdayPlanner> BirthdayPlanner { get; set; }
+
+    }
+}
---
+++ Lab36WebApi/Lab36WebApi/Models/BirthdayPlanner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab36WebApi.Models
+{
+    public class BirthdayPlanner
+    {
+        public int ID { get; set; }
+        public string Task { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
