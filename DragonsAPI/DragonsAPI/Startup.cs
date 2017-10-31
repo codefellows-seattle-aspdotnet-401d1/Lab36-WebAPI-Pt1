ccd4416807e6d8e25cebaa307f7ce452f70b90eb
Brandon Little <brlittle86@gmail.com>
2017-10-31 10:17:45 -0700

    added dependency injection to Startup, added Mvc to Startup

--- DragonsAPI/DragonsAPI/Startup.cs
+++ DragonsAPI/DragonsAPI/Startup.cs
@@ -6,15 +6,24 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
 
 namespace DragonsAPI
 {
     public class Startup
     {
+        public IConfiguration Configuration { get; }
+
+        // Construct a Startup object using a configuration passed in through dependency injection
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,6 +33,8 @@ namespace DragonsAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseMvc();
 
             app.Run(async (context) =>
             {
