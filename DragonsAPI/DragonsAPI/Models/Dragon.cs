b5ffc10254825a102b83766702553033415d5a58
Brandon Little <brlittle86@gmail.com>
2017-10-31 10:05:34 -0700

    created Dragon model

---
+++ DragonsAPI/DragonsAPI/Models/Dragon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonsAPI.Models
+{
+    public class Dragon
+    {
+        public int Id { get; set; }
+        public string Species { get; set; }
+        public string Description { get; set; }
+    }
+}
