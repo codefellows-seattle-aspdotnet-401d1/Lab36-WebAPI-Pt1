a513e6c2034a60df5ff26ea2dce011ae25bdbdb8
Thomas Reyes <thomasellison.r@gmail.com>
2017-10-31 11:23:05 -0700

    created constructor with context

--- Week8Tom/Week8Tom/Controllers/HeroesController.cs
+++ Week8Tom/Week8Tom/Controllers/HeroesController.cs
@@ -3,12 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Week8Tom.Data;
 
 namespace Week8Tom.Controllers
 {
     [Route("api/[controller]")]
     public class HeroesController : ControllerBase
     {
+        private readonly HeroStatsDbContext _context;
+
+        public HeroesController(HeroStatsDbContext context)
+        {
+            _context = context;
+        }
         //GET
         [HttpGet("{id:int?}")]
         public string Get(string id = "not found")
