dfddca95c7bb388c30b8fea43cc6b2a47dcced9d
Brandon Little <brlittle86@gmail.com>
2017-10-31 10:19:47 -0700

    added DragonsController and inherited from ControllerBase for it

---
+++ DragonsAPI/DragonsAPI/Controllers/DragonsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DragonsAPI.Controllers
+{
+    // Inherit from ControllerBase to keep code light by not including functionality not needed in an API format
+    public class DragonsController : ControllerBase
+    {
+    }
+}
