9665a14a7a1098c0470c9dae6874877362a5706a
George Gaylor <georgegaylor83@outlook.com>
2017-11-01 02:12:47 -0700

    Added Db Context

---
+++ Lab36George/Lab36George/Data/PartsDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Lab36George.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab36George.Data
+{
+    public class PartsDBContext : DbContext
+    {
+        public PartsDBContext(DbContextOptions<PartsDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Parts> Parts { get; set; }
+    }
+
+    
+}
