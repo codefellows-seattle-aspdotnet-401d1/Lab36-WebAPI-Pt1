0135670fdd73bb5c3e28b6f6fb095d9bb55adb92
George Gaylor <georgegaylor83@outlook.com>
2017-10-31 17:13:03 -0700

    Worked on delete

--- Lab36George/Lab36George.Test/UnitTest1.cs
+++ Lab36George/Lab36George.Test/UnitTest1.cs
@@ -1,4 +1,7 @@
+using Lab36George.Controllers;
+using Microsoft.AspNetCore.Mvc;
 using System;
+using System.Net;
 using Xunit;
 
 namespace Lab36George.Test
@@ -8,7 +11,17 @@ namespace Lab36George.Test
         [Fact]
         public void Test1()
         {
+            // Arrange
+            var controller = new PartsController();
 
+            // Act
+            var result = controller.Get(5);
+            ObjectResult statusCode = (ObjectResult)result;
+
+            // Assert
+            Assert.Equal(HttpStatusCode.OK, (HttpStatusCode)statusCode.StatusCode.Value);
+            // need to figure out the type
+            //Assert.IsType(typeof(figureoutthetype),);
         }
     }
 }
--- Lab36George/Lab36George/Controllers/PartsController.cs
+++ Lab36George/Lab36George/Controllers/PartsController.cs
@@ -8,7 +8,35 @@ namespace Lab36George.Controllers
 {
     [Route("api/[controller]")]
     public class PartsController : ControllerBase
-
     {
+        // Get
+        //[HttpGet]
+        
+        // Get all
+        // [HttpGet]
+
+        // Post
+        //[HttpPost]
+
+        // Put
+        //[HttpPut]
+
+        // Delete
+        //[HttpDelete("(id:int)")]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var result = _context.Parts.FirstOrDefault(d => d.id == id);
+        //    if ( result != null)
+        //    {
+        //        await _context.Parts.Remove(result);
+        //        await _context.SaveChangesAsync();
+        //        return Ok();
+        //    }
+        //    return BadRequest();
+        //}
+        
+
+
+
     }
 }
