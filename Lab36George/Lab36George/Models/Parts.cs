45b3a98d574b13db868a9e5f4f4aba85a8356766
George Gaylor <georgegaylor83@outlook.com>
2017-11-01 02:07:50 -0700

    created model and added dependancy injection to startup

---
+++ Lab36George/Lab36George/Models/Parts.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab36George.Models
+{
+    public class Parts
+    {
+        public int PartID { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; }
+        public int Quantity { get; set; }
+
+    }
+}
